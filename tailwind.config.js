3617e6f0b19c56b6df30ee24cf1f5c6de1392cc2
Anonoz Chong <hello@anonoz.com>
2021-12-11 18:42:09 +0800

    Step 3 - Add svelte to tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.svelte',
   ]
 }
