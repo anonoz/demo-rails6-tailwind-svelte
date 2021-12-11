76b031d5dd25d818880042250bdfed22792a2a7e
Anonoz Chong <hello@anonoz.com>
2021-12-11 16:20:50 +0800

    Disable db, storage, mailer in demo

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+# require "active_model/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
